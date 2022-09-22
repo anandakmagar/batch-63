d8e433508617dd8a991c6ef1341edddfa2a62dcb
Ananda Magar <anandmagar1990@gmail.com>
2022-09-21 19:30:36 -0600

    added

--- src/challenges/FactorialValue.java
+++ src/challenges/FactorialValue.java
@@ -6,14 +6,13 @@ public class FactorialValue {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
-        System.out.println("Enter the numbers for finding the factorial");
+        System.out.println("Enter the number for finding the factorial");
         int num = scnr.nextInt();
 
         int factorial = 1;
         for (int i = num; i >= 1; i--){
-            factorial = num*i;
-
+            factorial = factorial * i;
         }
-        System.out.println(factorial);
+        System.out.println(num + "! = " + factorial);
     }
 }
